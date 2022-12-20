269d20b235d610d8793f55a60298db0ea8db320d
LidorAvital <avitallidor8@hotmail.com>
2022-12-20 16:02:06 +0200

    Creating Admin Navbar complete

--- src/navigator/AdminNavigator.jsx
+++ src/navigator/AdminNavigator.jsx
@@ -4,14 +4,15 @@ import Actors from "../components/admin/Actors";
 import Dashboard from "../components/admin/Dashboard";
 import Movies from "../components/admin/Movies";
 import AdminNavbar from "../components/admin/AdminNavbar";
-
 import NotFound from "../components/NotFound";
+import Header from "../components/admin/Header";
 
 export default function AdminNavigator() {
   return (
-    <div className="flex">
+    <div className="flex dark:bg-primary bg-white">
       <AdminNavbar />
       <div className="flex-1 p-2 max-w-screen-xl">
+        <Header />
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/movies" element={<Movies />} />

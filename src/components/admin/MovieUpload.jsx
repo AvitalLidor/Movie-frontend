c8a3d009508a5e035ee9bbf6833e5d008a0def79
LidorAvital <avitallidor8@hotmail.com>
2022-12-21 20:24:38 +0200

    Moive Upload

--- src/components/admin/MovieUpload.jsx
+++ src/components/admin/MovieUpload.jsx
@@ -4,21 +4,55 @@ import { FileUploader } from "react-drag-drop-files";
 import { AiOutlineCloudUpload } from "react-icons/ai";
 import { uploadTrailer } from "../../api/movie";
 import { useNotification } from "../../hooks";
+import MovieForm from "./MovieForm";
 
 export default function MovieUpload() {
-  const [videoSelected, setVideoSelected] = useState(true);
+  const [videoSelected, setVideoSelected] = useState(false);
+  const [videoUploaded, setVideoUploaded] = useState(false);
+  const [uploadProgress, setUploadProgress] = useState(0);
+  const [videoInfo, setVideoInfo] = useState({});
+
+  const [movieInfo, setMovieInfo] = useState({
+    title: "",
+    cast: [],
+    trailer: {
+      url: "",
+      public_id: "",
+    },
+  });
+
   const { updateNotification } = useNotification();
 
   const handleTypeError = (error) => {
     updateNotification("error", error);
   };
 
-  const handleChange = async (file) => {
+  const handleUploadTrailer = async (data) => {
+    const { error, url, public_id } = await uploadTrailer(
+      data,
+      setUploadProgress
+    );
+    if (error) return updateNotification("error", error);
+    setVideoUploaded(true);
+
+    setVideoInfo({ url, public_id });
+  };
+
+  console.log(videoInfo);
+
+  const handleChange = (file) => {
     const formData = new FormData();
     formData.append("video", file);
 
-    const res = await uploadTrailer(formData);
-    console.log(res);
+    setVideoSelected(true);
+    handleUploadTrailer(formData);
+  };
+
+  const getUploadProgressValue = () => {
+    if (!videoUploaded && uploadProgress >= 100) {
+      return "Processing";
+    }
+    return `Upload progress  ${uploadProgress}%`;
   };
 
   return (
@@ -26,28 +60,18 @@ export default function MovieUpload() {
       className="fixed inset-0 dark:bg-white dark:bg-opacity-50 bg-primary bg-opacity-50 backdrop-blur-sm 
     flex items-center justify-center"
     >
-      <div className="dark:bg-primary bg-white rounded w-[45rem] h-[40rem] overflow-auto">
-        <div className="p-2">
-          <div
-            className="dark:bg-secondary bg-white drop-shadow-lg rounded p-3
-          "
-          >
-            <div className="relative h-3 dark:bg-dark-subtle bg-light-subtle overflow-hidden">
-              <div
-                style={{ width: "80%" }}
-                className="h-full  absolute left-0 dark:bg-white bg-secondary"
-              />
-            </div>
-            <p className="font-semibold dark:text-dark-subtle text-light-subtle animate-pulse mt-1">
-              Upload progress 80%
-            </p>
-          </div>
-        </div>
+      <div className="dark:bg-primary bg-white rounded w-[45rem] h-[40rem] overflow-auto p-2">
+        {/* <UploadProgress
+          visible={!videoUploaded && videoSelected}
+          message={getUploadProgressValue()}
+          width={uploadProgress}
+        />
         <TrailerSelector
           visible={!videoSelected}
           onTypeError={handleTypeError}
           handleChange={handleChange}
-        />
+        /> */}
+        <MovieForm />
       </div>
     </div>
   );
@@ -72,6 +96,23 @@ const TrailerSelector = ({ visible, handleChange, onTypeError }) => {
           <p>Drop your file here!</p>
         </div>
       </FileUploader>
+    </div>
+  );
+};
+
+const UploadProgress = ({ width, message, visible }) => {
+  if (!visible) return null;
+  return (
+    <div className="dark:bg-secondary bg-white drop-shadow-lg rounded p-3">
+      <div className="relative h-3 dark:bg-dark-subtle bg-light-subtle overflow-hidden">
+        <div
+          style={{ width: width + "%" }}
+          className="h-full  absolute left-0 dark:bg-white bg-secondary"
+        />
+      </div>
+      <p className="font-semibold dark:text-dark-subtle text-light-subtle animate-pulse mt-1">
+        {message}
+      </p>
     </div>
   );
 };

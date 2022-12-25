91c703840ac884bbd3aa70547e693163413b3951
LidorAvital <avitallidor8@hotmail.com>
2022-12-25 13:33:19 +0200

    Refactor Live Search

--- src/components/LiveSearch.jsx
+++ src/components/LiveSearch.jsx
@@ -1,46 +1,17 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { forwardRef, useEffect, useRef, useState } from "react";
 import { commonInputClasses } from "../utils/theme";
 
-export const results = [
-  {
-    id: "1",
-    avatar:
-      "https://images.unsplash.com/photo-1643713303351-01f540054fd7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
-    name: "John Doe",
-  },
-  {
-    id: "2",
-    avatar:
-      "https://images.unsplash.com/photo-1643883135036-98ec2d9e50a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
-    name: "Chandri Anggara",
-  },
-  {
-    id: "3",
-    avatar:
-      "https://images.unsplash.com/photo-1578342976795-062a1b744f37?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
-    name: "Amin RK",
-  },
-  {
-    id: "4",
-    avatar:
-      "https://images.unsplash.com/photo-1564227901-6b1d20bebe9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
-    name: "Edward Howell",
-  },
-  {
-    id: "5",
-    avatar:
-      "https://images.unsplash.com/photo-1578342976795-062a1b744f37?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
-    name: "Amin RK",
-  },
-  {
-    id: "6",
-    avatar:
-      "https://images.unsplash.com/photo-1564227901-6b1d20bebe9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=80",
-    name: "Edward Howell",
-  },
-];
-
-export default function LiveSearch() {
+export default function LiveSearch({
+  value = "",
+  placeholder = "",
+  results = [],
+  selectedResultStyle,
+  resultContainerStyle,
+  inputStyle,
+  renderItem = null,
+  onChange = null,
+  onSelect = null,
+}) {
   const [displaySearch, setDisplaySearch] = useState(false);
   const [focusedIndex, setFocusedIndex] = useState(-1);
 
@@ -58,7 +29,7 @@ export default function LiveSearch() {
   };
 
   const handleSelection = (selectedItem) => {
-    console.log(selectedItem);
+    onSelect(selectedItem);
   };
 
   const handleKeyDown = ({ key }) => {
@@ -79,6 +50,12 @@ export default function LiveSearch() {
     setFocusedIndex(nextCount);
   };
 
+  const getInputStyle = () => {
+    return inputStyle
+      ? inputStyle
+      : commonInputClasses + " border-2 rounded p-1 text-lg ";
+  };
+
   return (
     <div
       tabIndex={1}
@@ -88,23 +65,43 @@ export default function LiveSearch() {
     >
       <input
         type="text"
-        className={commonInputClasses + " border-2 rounded p-1 text-lg"}
-        placeholder="Search profile"
+        className={getInputStyle()}
+        placeholder={placeholder}
         onFocus={handleOnFocus}
-        // onBlur={handleOnBlur}
-        // onKeyDown={handleKeyDown}
+        value={value}
+        onChange={onChange}
       />
       <SearchResults
         results={results}
         visible={displaySearch}
         focusedIndex={focusedIndex}
         onSelect={handleSelection}
+        renderItem={renderItem}
+        resultContainerStyle={resultContainerStyle}
+        selectedResultStyle={selectedResultStyle}
       />
     </div>
   );
 }
 
-const SearchResults = ({ visible, results = [], focusedIndex, onSelect }) => {
+// const renderItem = ({ id, name, avatar }) => {
+//   return (
+//     <div className="flex">
+//       <img src={avatar} alt="" />
+//       <p>{name}</p>
+//     </div>
+//   );
+// };
+
+const SearchResults = ({
+  visible,
+  results = [],
+  focusedIndex,
+  onSelect,
+  renderItem,
+  resultContainerStyle,
+  selectedResultStyle,
+}) => {
   const resultContainer = useRef();
 
   useEffect(() => {
@@ -119,28 +116,49 @@ const SearchResults = ({ visible, results = [], focusedIndex, onSelect }) => {
   return (
     <div className="absolute right-0 left-0 top-10 bg-white dark:bg-secondary shadow-md p-2 max-h-64 space-y-2 mt-1 overflow-auto custom-scroll-bar">
       {results.map((result, index) => {
-        const { id, name, avatar } = result;
+        const getSelectedClass = () => {
+          return selectedResultStyle
+            ? selectedResultStyle
+            : "dark:bg-dark-subtle bg-light-subtle";
+        };
         return (
-          <div
-            onMouseDown={() => onSelect(result)}
-            ref={index === focusedIndex ? resultContainer : null}
-            key={id}
-            className={
-              (index === focusedIndex
-                ? "dark:bg-dark-subtle bg-light-subtle"
-                : "") +
-              " cursor-pointer rounded overflow-hidden dark:hover:bg-dark-subtle hover:bg-light-subtle transition flex space-x-2"
+          <ResultCard
+            key={result.id}
+            item={result}
+            renderItem={renderItem}
+            resultContainerStyle={resultContainerStyle}
+            selectedResultStyle={
+              index === focusedIndex ? getSelectedClass() : ""
             }
-          >
-            <img
-              src={avatar}
-              alt={name}
-              className="w-16 h-16 rounded object-cover"
-            />
-            <p className="dark:text-white font-semibold ">{name}</p>
-          </div>
+            onMouseDown={() => onSelect(result)}
+          />
         );
       })}
     </div>
   );
 };
+
+const ResultCard = forwardRef((props, ref) => {
+  const {
+    item,
+    renderItem,
+    resultContainerStyle,
+    selectedResultStyle,
+    onMouseDown,
+  } = props;
+
+  const getClasses = () => {
+    if (resultContainerStyle)
+      return resultContainerStyle + " " + selectedResultStyle;
+
+    return (
+      selectedResultStyle +
+      " cursor-pointer rounded overflow-hidden dark:hover:bg-dark-subtle hover:bg-light-subtle transition"
+    );
+  };
+  return (
+    <div onMouseDown={onMouseDown} ref={ref} className={getClasses()}>
+      {renderItem(item)}
+    </div>
+  );
+});

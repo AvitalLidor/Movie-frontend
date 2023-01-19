d57cd6c610198eae4564472ca40349613db3ecb1
LidorAvital <avitallidor8@hotmail.com>
2023-01-19 22:47:41 +0200

    Adding review

--- src/components/user/SingleMovie.jsx
+++ src/components/user/SingleMovie.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { getSingleMovie } from "../../api/movie";
-import { useNotification } from "../../hooks";
+import { useAuth, useNotification } from "../../hooks";
 import Container from "../Container";
+import AddRatingModal from "../models/AddRatingModal";
 import RatingStar from "../RatingStar";
+import RelatedMovies from "../RelatedMovies";
 
 //converting thousand to k
 const convertReviewCount = (count = 0) => {
@@ -20,10 +22,14 @@ const convertDate = (date = "") => {
 
 export default function SingleMovie() {
   const [ready, setReady] = useState(false);
+  const [showRatingModal, setShowRatingModal] = useState(false);
   const [movie, setMovie] = useState({});
 
   const { updateNotification } = useNotification();
+  const navigate = useNavigate();
+  const { authInfo } = useAuth();
   const { movieId } = useParams();
+  const { isLoggedIn } = authInfo;
 
   const fetchMovie = async () => {
     const { error, movie } = await getSingleMovie(movieId);
@@ -32,6 +38,20 @@ export default function SingleMovie() {
     setReady(true);
     setMovie(movie);
   };
+
+  const handleOnRateMovie = () => {
+    if (!isLoggedIn) return navigate("/auth/signin");
+    setShowRatingModal(true);
+  };
+
+  const hideRatingModal = () => {
+    setShowRatingModal(false);
+  };
+
+  const handleOnRatingSuccess = (reviews) => {
+    setMovie({ ...movie, reviews: { ...reviews } });
+  };
+
   useEffect(() => {
     if (movieId) fetchMovie();
   }, [movieId]);
@@ -53,10 +73,12 @@ export default function SingleMovie() {
     storyLine,
     language,
     releaseDate,
+    type,
     director = {},
     reviews = {},
     writers = [],
     cast = [],
+    genres = [],
   } = movie;
 
   return (
@@ -79,6 +101,7 @@ export default function SingleMovie() {
             <button
               className="text-highlight dark:text-highlight-dark hover:underline"
               type="button"
+              onClick={handleOnRateMovie}
             >
               Rate the movie
             </button>
@@ -150,8 +173,73 @@ export default function SingleMovie() {
               {convertDate(releaseDate)}
             </p>
           </div>
+
+          <div className="flex">
+            <p className="text-light-subtle dark:text-dark-subtle font-semibold mr-2">
+              Genres:
+            </p>
+
+            <div className="flex space-x-2">
+              {genres.map((g) => {
+                return (
+                  <p
+                    key={g}
+                    className="text-highlight dark:text-highlight-dark "
+                  >
+                    {g}
+                  </p>
+                );
+              })}
+            </div>
+          </div>
+
+          <div className="flex space-x-2">
+            <p className="text-light-subtle dark:text-dark-subtle font-semibold">
+              Type:
+            </p>
+            <p className="text-highlight dark:text-highlight-dark">{type}</p>
+          </div>
+        </div>
+
+        <div className="mt-5">
+          <h1 className="text-light-subtle dark:text-dark-subtle font-semibold text-2xl mb-2">
+            Cast:
+          </h1>
+          <div className="grid grid-cols-10">
+            {cast.map((c) => {
+              return (
+                <div key={c.profile.id} className="flex flex-col items-center">
+                  <img
+                    className="w-24 h-24 aspect-square object-cover rounded-full"
+                    src={c.profile.avatar}
+                    alt=""
+                  />
+
+                  <p className="text-highlight dark:text-highlight-dark hover:underline cursor-pointer">
+                    {c.profile.name}
+                  </p>
+                  <span className="text-light-subtle dark:text-dark-subtle text-sm">
+                    as
+                  </span>
+                  <p className="text-light-subtle dark:text-dark-subtle">
+                    {c.roleAs}
+                  </p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+
+        <div className="mt-3">
+          <RelatedMovies movieId={movieId} />
         </div>
       </Container>
+
+      <AddRatingModal
+        visible={showRatingModal}
+        onClose={hideRatingModal}
+        onSuccess={handleOnRatingSuccess}
+      />
     </div>
   );
 }
